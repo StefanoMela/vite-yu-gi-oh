1af8e8ed8b52e41411fda1c25b4243fe1d518ae9
StefanoMela <stefano.mela25@gmail.com>
2023-09-12 15:25:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import axios from "axios";
+import { store } from "./data/store.js";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  methods: {
+    fetchCharacters(endpoint) {
+      axios.get(endpoint).then((response) => {
+
+        store.cards = response.data.data;
+        console.log(store.cards);
+
+      });
+    },
+  },
+
+  created() {
+    this.fetchCharacters("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0");
+  },
+};
+</script>
+
+<template>
+  <h1>Prova test</h1>
+</template>
+
+<style lang="scss" scoped></style>
